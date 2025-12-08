cb2ce0de11f46fec656034bb7d8dec8e50746559
Nadeen-Gunathilake <129357314+Nadeen-Gunathilake@users.noreply.github.com>
2025-12-08 22:36:10 +0530

    Delete lexium_mobile directory

--- lexium_mobile/README.md
+++
@@ -1,3 +0,0 @@
-# lexium_mobile
-
-A new Flutter project.
--- lexium_mobile/android/app/src/main/kotlin/com/example/lexium_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lexium_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
