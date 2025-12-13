3c5aaa3617a9ff47bcb22189936d594c0ae3afb7
Nadeen-Gunathilake <nadeen.guna05@gmail.com>
2025-12-13 16:22:15 +0530

    pages done
    chatbot_page
    login_page
    setings_page
    signup_page

---
+++ lexium_mobile/android/lib/pages/chatbot_page.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:lexium_mobile/components/chat_field.dart';
+
+class ChatBotPage extends StatefulWidget {
+  const ChatBotPage({super.key});
+
+  @override
+  State<ChatBotPage> createState() => _ChatBotPageState();
+}
+
+class _ChatBotPageState extends State<ChatBotPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lexium AI Assistant'),
+        centerTitle: true,
+        actions: [
+          PopupMenuButton<String>(
+            icon: const Icon(Icons.more_vert),
+            offset: const Offset(0, 52),
+            onSelected: (value) {
+              if (value == 'settings') {
+                Navigator.pushNamed(context, '/settings');
+              }
+            },
+            itemBuilder: (BuildContext context) => [
+              const PopupMenuItem<String>(
+                value: 'settings',
+                child: Row(
+                  children: [
+                    Icon(Icons.settings),
+                    SizedBox(width: 8),
+                    Text('Settings'),
+                    SizedBox(width: 10),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.grey[200],
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(width: double.infinity),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Welcome to Lexium AI Assistant',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    const SizedBox(height: 10),
+
+                    const Text(
+                      'You can ask me about our rights as a tenant, contract law, or other legal topics .Here are some examples to get started.',
+                      style: TextStyle(fontSize: 14),
+                      textAlign: TextAlign.center,
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    ElevatedButton(
+                      onPressed: () {
+                        debugPrint("Tenant Rights Pressed");
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        minimumSize: Size(200, 50),
+                      ),
+
+                      child: Text("Tenant Rights"),
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        debugPrint("Small claims court Pressed");
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        minimumSize: Size(200, 50),
+                      ),
+
+                      child: Text("Small claims court"),
+                    ),
+
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const ChatField(),
+        ],
+      ),
+    );
+  }
+}
